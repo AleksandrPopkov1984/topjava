97c970a36dcd776c300c3db9794019ee337c0754
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-10-07 22:32:50 +0300

    add display list of meals implementation

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -18,6 +22,8 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        response.sendRedirect("meals.jsp");
+        List<MealTo> mealTo = MealsUtil.filteredByStreams(MealsUtil.getListOfMeals(), LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
+        request.setAttribute("meals", mealTo);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="tags" tagdir="/WEB-INF/tags" %>
+
 <html lang="ru">
 <head>
     <title>Users</title>
@@ -6,6 +9,25 @@
 <body>
 <h3><a href="index.html">Home</a></h3>
 <hr>
-<h2>meals</h2>
+<h2>Meals</h2>
+
+<table border="1">
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+
+    <c:forEach var="meal" items="${meals}">
+        <tr style="color:${meal.excess ? 'red' : 'green'}">
+            <td><tags:localDate date="${meal.dateTime}" pattern="yyyy-MM-dd HH:mm"/></td>
+            <td>${meal.description}</td>
+            <td>${meal.calories}</td>
+        </tr>
+
+    </c:forEach>
+
+</table>
+
 </body>
 </html>
