f7e01a843ecba745baf5f09461a39c5686567bd4
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-10-21 09:52:18 +0300

    add sql-scripts of meals tables

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,7 +1,10 @@
 DELETE
 FROM user_roles;
 DELETE
+FROM meals;
+DELETE
 FROM users;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -12,3 +15,9 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2022-10-20 08:00:00-00', 'Завтрак', 500, 100001),
+       ('2022-10-20 19:30:45-07', 'Ужин', 1000, 100001);
+
+
