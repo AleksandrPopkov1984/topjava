5064363d420f285e24c2de368e3246fb75164677
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-11-28 08:20:48 +0300

    add ResourceControllerTest and improve getMeals Test in RootControllerTest

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
@@ -38,18 +39,12 @@ class RootControllerTest extends AbstractControllerTest {
 
     @Test
     void getMeals() throws Exception {
+        List<MealTo> expectedValue = MealsUtil.getTos(meals, DEFAULT_CALORIES_PER_DAY);
         perform(get("/meals"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("meals"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals",
-                        new AssertionMatcher<List<MealTo>>() {
-                            @Override
-                            public void assertion(List<MealTo> actual) throws AssertionError {
-                                MEALTO_MATCHER.assertMatch(actual, getTos(meals, DEFAULT_CALORIES_PER_DAY));
-                            }
-                        }
-                ));
+                .andExpect(model().attribute("meals", expectedValue));
     }
 }
