9ae6b57ac095c463011c4daeb76f11b860b44c86
Alexandr Popkov <alekspopkov1984@gmail.com>
2022-12-05 13:19:00 +0300

    transfer meals.jsp to Datatables

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,57 @@
+const mealAjaxUrl = "ui/meals/";
+
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "desc"
+                ]
+            ]
+        })
+    );
+});
+
+$('#filterDetails').submit(function () {
+    filterTable();
+    return false;
+});
+
+function filterTable() {
+    $.ajax({
+        type: "GET",
+        url: ctx.ajaxUrl + 'filter',
+        data: $('#filterDetails').serialize(),
+        success: updateTableByData
+    });
+}
+
+function updateTableByData(data) {
+    ctx.datatableApi.clear().rows.add(data).draw();
+}
